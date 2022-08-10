77fd7b82773b2d1cf25d0c4cdc99758e7eba4f2b
SunbaeLee <tnsqo1126@naver.com>
2022-08-10 22:48:17 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace FindFile_Sun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            //this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.splitter2 = new System.Windows.Forms.Splitter();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.laResult = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.rad_Excel = new System.Windows.Forms.RadioButton();
+            this.radAll = new System.Windows.Forms.RadioButton();
+            this.radPPT = new System.Windows.Forms.RadioButton();
+            this.radWord = new System.Windows.Forms.RadioButton();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.panel2 = new System.Windows.Forms.Panel();
+           // ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+           // this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+           //// this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+           // this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+           // this.pictureBox1.Name = "pictureBox1";
+           // this.pictureBox1.Size = new System.Drawing.Size(3, 518);
+           // this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+           // this.pictureBox1.TabIndex = 0;
+           // this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.splitter2);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(441, 518);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.listView1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 241);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(441, 277);
+            this.panel4.TabIndex = 8;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(441, 277);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.DoubleClick += new System.EventHandler(this.DbClickItem);
+            // 
+            // splitter2
+            // 
+            this.splitter2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitter2.Location = new System.Drawing.Point(0, 238);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(441, 3);
+            this.splitter2.TabIndex = 7;
+            this.splitter2.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.btnSelect);
+            this.panel3.Controls.Add(this.txtPath);
+            this.panel3.Controls.Add(this.laResult);
+            this.panel3.Controls.Add(this.btnSearch);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.dateTimePicker2);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.dateTimePicker1);
+            this.panel3.Controls.Add(this.txtKeyword);
+            this.panel3.Controls.Add(this.rad_Excel);
+            this.panel3.Controls.Add(this.radAll);
+            this.panel3.Controls.Add(this.radPPT);
+            this.panel3.Controls.Add(this.radWord);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(441, 238);
+            this.panel3.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("맑은 고딕", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Developer : Sunbae★";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSelect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelect.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSelect.ForeColor = System.Drawing.Color.White;
+            this.btnSelect.Location = new System.Drawing.Point(341, 34);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(73, 25);
+            this.btnSelect.TabIndex = 9;
+            this.btnSelect.Text = "선택";
+            this.btnSelect.UseVisualStyleBackColor = false;
+            this.btnSelect.Click += new System.EventHandler(this.SetPath);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(98, 34);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(237, 21);
+            this.txtPath.TabIndex = 8;
+            // 
+            // laResult
+            // 
+            this.laResult.AutoSize = true;
+            this.laResult.Location = new System.Drawing.Point(37, 213);
+            this.laResult.Name = "laResult";
+            this.laResult.Size = new System.Drawing.Size(0, 12);
+            this.laResult.TabIndex = 7;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Location = new System.Drawing.Point(340, 172);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 30);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.SearchButton);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(18, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "찾을 파일 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(235, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "에서";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(36, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "날짜별 :";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CalendarFont = new System.Drawing.Font("맑은 고딕", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(267, 75);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(147, 21);
+            this.dateTimePicker2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(23, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "드라이브 :";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("맑은 고딕", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(97, 75);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(132, 21);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtKeyword.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtKeyword.Location = new System.Drawing.Point(97, 116);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(317, 23);
+            this.txtKeyword.TabIndex = 1;
+            this.txtKeyword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EnterClick);
+            this.txtKeyword.TextChanged += new System.EventHandler(this.ClearExample);
+            // 
+            // rad_Excel
+            // 
+            this.rad_Excel.AutoSize = true;
+            this.rad_Excel.Location = new System.Drawing.Point(164, 145);
+            this.rad_Excel.Name = "rad_Excel";
+            this.rad_Excel.Size = new System.Drawing.Size(71, 16);
+            this.rad_Excel.TabIndex = 2;
+            this.rad_Excel.TabStop = true;
+            this.rad_Excel.Text = "엑셀파일";
+            this.rad_Excel.UseVisualStyleBackColor = true;
+            // 
+            // radAll
+            // 
+            this.radAll.AutoSize = true;
+            this.radAll.Location = new System.Drawing.Point(100, 145);
+            this.radAll.Name = "radAll";
+            this.radAll.Size = new System.Drawing.Size(47, 16);
+            this.radAll.TabIndex = 2;
+            this.radAll.TabStop = true;
+            this.radAll.Text = "전체";
+            this.radAll.UseVisualStyleBackColor = true;
+            // 
+            // radPPT
+            // 
+            this.radPPT.AutoSize = true;
+            this.radPPT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radPPT.Location = new System.Drawing.Point(251, 145);
+            this.radPPT.Name = "radPPT";
+            this.radPPT.Size = new System.Drawing.Size(70, 16);
+            this.radPPT.TabIndex = 2;
+            this.radPPT.TabStop = true;
+            this.radPPT.Text = "PPT파일";
+            this.radPPT.UseVisualStyleBackColor = true;
+            // 
+            // radWord
+            // 
+            this.radWord.AutoSize = true;
+            this.radWord.Location = new System.Drawing.Point(340, 145);
+            this.radWord.Name = "radWord";
+            this.radWord.Size = new System.Drawing.Size(71, 16);
+            this.radWord.TabIndex = 2;
+            this.radWord.TabStop = true;
+            this.radWord.Text = "워드파일";
+            this.radWord.UseVisualStyleBackColor = true;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(441, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 518);
+            this.splitter1.TabIndex = 2;
+            this.splitter1.TabStop = false;
+            // 
+            // panel2
+            // 
+            //this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(444, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(3, 518);
+            this.panel2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(447, 518);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "파일찾기 프로그램 - SunBae";
+            this.Load += new System.EventHandler(this.Form1_Load);
+           // ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+       // private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton radAll;
+        private System.Windows.Forms.RadioButton radWord;
+        private System.Windows.Forms.RadioButton radPPT;
+        private System.Windows.Forms.RadioButton rad_Excel;
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Splitter splitter2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label laResult;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,314 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace FindFile_Sun
+{
+
+
+    public partial class Form1 : Form 
+    {
+        static FindClass fc = new FindClass();
+
+        //private List<Bitmap> list = new List<Bitmap>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            Icon = Properties.Resources.computer_laptop_notebook_screen_technology_icon_0;
+             radAll.Checked = true;
+         }
+
+        #region 검색어 입력하려고 할 하면 예시지워주기
+        private void ClearExample(object sender, EventArgs e)
+        {
+            string strKey = txtKeyword.Text;
+
+           if (strKey.IndexOf("ex)") > 0 )
+            {
+                Console.WriteLine("포함 됨");
+            }
+        }
+    
+        #endregion
+
+        #region 처음 폼 로드 될 때 
+       // public DispatcherTimer Timer = new DispatcherTimer();   // WindowBase.dll
+        public Random Ran = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Timer.Interval = TimeSpan.FromSeconds(3);
+            //Timer.Tick += new EventHandler(SlideShow_Image);
+            //Timer.Start();
+
+            listView1.View = View.Details;
+            listView1.CheckBoxes = true;
+            listView1.GridLines = true;
+            listView1.MultiSelect = true;
+
+            listView1.Columns.Add(@"파일명",150);
+            listView1.Columns.Add(@"크기", 100);
+            listView1.Columns.Add(@"확장자");
+            listView1.Columns.Add(@"최근수정날짜");
+            listView1.Columns.Add(@"경로");
+
+            // 예시 표현 및 폰트 컬러 설정 
+            txtKeyword.Text = @"ex) *연말정산*";
+            txtKeyword.Font = new Font("맑은 고딕", 9, FontStyle.Italic);
+            txtKeyword.ForeColor = Color.Gray;
+        }
+        #endregion
+
+        #region 파일선택창
+        private void SetPath(object sender, EventArgs e)
+        {
+            // 외부 api 사용함
+            var dir = new CommonOpenFileDialog();       // 선언 var는 좀 더 공부해야 함
+
+            dir.IsFolderPicker = true;  // 폴더선택 허용
+
+            if (dir.ShowDialog() == CommonFileDialogResult.Ok)      // 폴더만 선택 한다면
+            {
+                System.Diagnostics.Debug.Print("폴더만 눌렀음");
+                System.Diagnostics.Debug.Print(dir.FileName);
+                txtPath.Text = dir.FileName;    // 선택한 path 넣어줌
+            }
+            else // 취소하거나 esc했을 경우?
+            {
+                return;
+            }
+            txtKeyword.Focus();
+        }
+
+        #endregion
+
+        #region Enter Key 했을 때
+        private void EnterClick(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(13))
+            {
+                SearchButton(sender, e);
+            }
+        }
+        #endregion
+
+        #region 파일 찾기 기능
+        private void SearchButton(object sender, EventArgs e)
+        {
+            if (txtKeyword.Text.Trim().Equals(""))  // 입력 된 검색어가 없다면
+            {
+                MessageBox.Show("검색어를 입력해주세요.", "lee.sunbae", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (txtPath.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("경로를 지정해주세요.", "lee.sunbae", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            listView1.BeginUpdate();
+
+            FindClass fc = new FindClass(txtPath.Text, txtKeyword.Text);
+
+            Thread th1 = new Thread(new ThreadStart(fc.GoFind));
+            th1.Start();
+            th1.Join(); // 다른 스레드 호출 방지
+
+            IEnumerable<FileInfo> files = fc.getResult();
+
+            pushDel pd = new pushDel(pushData);
+                       
+            Thread th2 = new Thread((delegate () { pushData(files); }));
+
+ 
+            th2.Start();
+
+            //스레드가모두멈추기까지대기
+            //Thread.Sleep(100);
+
+            listView1.EndUpdate();
+        }
+
+        delegate void pushDel(IEnumerable<FileInfo> files);
+
+        // 넣기
+         void pushData(IEnumerable<FileInfo> files)
+        {
+
+            foreach (FileInfo file in files)
+            {
+                ListViewItem item = new ListViewItem(file.Name, 0);       // 파일 명
+                long aSize = Convert.ToInt64(file.Length.ToString());
+                item.SubItems.Add(FormatBytes(aSize));                    // 파일 크기
+                item.SubItems.Add(file.Extension.ToString());                // 파일 종류 (확장자)
+                item.SubItems.Add(file.LastAccessTime.ToString());        // 파일 마지막 수정날짜
+                item.SubItems.Add(file.FullName.ToString());                  // 경로
+
+                if (listView1.InvokeRequired == true)
+                {  // 익명 메소드를 이용하여 invoke 처리 
+                    listView1.Invoke(new MethodInvoker(delegate () { listView1.Items.Add(item); }));
+                }
+                else
+                {
+                    listView1.Items.Add(item);
+                }
+
+            }
+
+        }
+
+        private void FindFiles(string dir, string keyword)
+        {
+            // 경로 저장
+            //string[] files = System.IO.Directory.GetFiles(dir, keyword,SearchOption.AllDirectories);
+            DirectoryInfo di = new DirectoryInfo(dir);
+            //FileInfo[] files = di.GetFiles(keyword, SearchOption.AllDirectories);
+
+            // 디렉토리 IEnumerable 사용하여 검색
+            IEnumerable<FileInfo> files = di.GetFiles(keyword, SearchOption.AllDirectories);
+
+            // IEnumerable을 이용하면 Fileinfo의 length 가 없기 때문에, 아래와 같이 구함.
+            int c = files.Count(x => !x.Attributes.HasFlag(FileAttributes.ReparsePoint));
+
+
+            // 결과 표시
+            if (c > 0)   // 검색 결과가 있다면
+            {
+                laResult.Text = "검색 결과 : " + c + " 건이 검색되었습니다.";
+                listView1.Items.Clear();
+            }
+            else
+            {
+                laResult.Text = "검색 결과 : " + " 검색 결과가 없습니다.";
+                listView1.Items.Clear();
+            }
+
+            // 구조 변경 해야 함
+            // 현 : 반복하여 List View에 바로 넣음
+            // 후 : ListOf() 에 담아서 한번에 Binding 할 예정
+
+            foreach (FileInfo file in files)
+            {
+                ListViewItem item = new ListViewItem(file.Name, 0);       // 파일 명
+                long aSize = Convert.ToInt64(file.Length.ToString());
+                item.SubItems.Add(FormatBytes(aSize));                    // 파일 크기
+                item.SubItems.Add(file.Extension.ToString());                // 파일 종류 (확장자)
+                item.SubItems.Add(file.LastAccessTime.ToString());        // 파일 마지막 수정날짜
+                item.SubItems.Add(file.FullName.ToString());                  // 경로
+
+                listView1.Items.Add(item);
+            }
+
+        }
+
+        #endregion
+
+        #region 아이템 더블클릭 시 경로 열어주기
+
+        private void DbClickItem(object sender, EventArgs e)
+        {
+            string a = listView1.SelectedItems[0].SubItems[4].Text;
+
+            System.Diagnostics.Process.Start(a);
+        }
+
+
+        #endregion
+
+        #region 바이트 변환 함수
+        public string FormatBytes(long bytes)
+        {
+            const int scale = 1024;
+            string[] orders = new string[] { "GB", "MB", "KB", "Bytes" };
+            long max = (long)Math.Pow(scale, orders.Length - 1);
+
+            foreach (string order in orders)
+            {
+                if (bytes > max)
+                    return string.Format("{0:##.##} {1}", decimal.Divide(bytes, max), order);
+
+                max /= scale;
+            }
+            return "0 Bytes";
+        }
+        #endregion
+
+        #region 이미지슬라이드쇼
+        
+        private void SlideShow_Image(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int _index = r.Next(1, 4);
+            //pictureBox1.Image = new Bitmap(list[_index]);
+
+            byte[] image;
+
+
+
+
+
+
+        }
+
+        #endregion
+
+
+    }
+    class FindClass
+    {
+        private string path;
+        private string key;
+        private IEnumerable<FileInfo> files;
+
+        public IEnumerable<FileInfo> getResult() { return this.files; }
+
+        public FindClass() { }
+
+        public FindClass(string path, string key)   // file info
+        {
+            this.path = path; this.key = key;
+        }
+
+        public void GoFind()    // 찾기
+        {
+            lock (this)
+            {
+                // 경로
+                DirectoryInfo di = new DirectoryInfo(path);
+                //FileInfo[] files = di.GetFiles(keyword, SearchOption.AllDirectories);
+
+                // 디렉토리 IEnumerable 사용하여 검색
+                IEnumerable<FileInfo> files = di.GetFiles(key, SearchOption.AllDirectories);
+
+                // IEnumerable을 이용하면 Fileinfo의 length 가 없기 때문에, 아래와 같이 구함.
+                int c = files.Count(x => !x.Attributes.HasFlag(FileAttributes.ReparsePoint));
+
+                // 결과 표시
+                if (c > 0)   // 검색 결과가 있다면
+                {
+                    Console.WriteLine("검색 결과 : " + c + " 건이 검색되었습니다.");
+                }
+                else
+                {
+                    Console.WriteLine("검색 결과 : " + " 검색 결과가 없습니다.");
+                }
+
+                // 여기서 저장
+                this.files = files;
+
+            }
+
+        }
+
+    }
+}
